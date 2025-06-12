38a6c0b84e401d4d4240fd6e89576aaf381fe48c
Lyndsay.may <Lyndsay.may@libertymutual.com>
2025-06-12 13:16:16 -0400

    password masking

--- src/components/CustomerList.js
+++ src/components/CustomerList.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 
-function CustomerList({ customers, selectedId, onSelect }) {
+function CustomerList({ customers, selectedId, onSelect, }) {
   return (
     <div className="boxed">
       <h4>Customer List</h4>
@@ -10,7 +10,7 @@ function CustomerList({ customers, selectedId, onSelect }) {
           <tr>
             <th>Name</th>
             <th>Email</th>
-            <th>Pass</th>
+            <th>Password</th>
           </tr>
         </thead>
         <tbody>
@@ -22,7 +22,7 @@ function CustomerList({ customers, selectedId, onSelect }) {
             >
               <td>{item.name}</td>
               <td>{item.email}</td>
-              <td>{item.password}</td>
+              <td>{'*'.repeat(item.password.length)}</td>
             </tr>
           ))}
         </tbody>
--- src/components/ErrorHandling.js
+++ src/components/ErrorHandling.js
@@ -8,3 +8,17 @@ function checkBlankForm() {
     log("in onSaveClick()");
   };}
     
+  const [errorMessage, setErrorMessage] = useState('')
+
+    const validate = (value) => {
+
+        if (validator.isStrongPassword(value, {
+            minLength: 8, minLowercase: 1,
+            minUppercase: 1, minNumbers: 1, minSymbols: 1
+        })) {
+            setErrorMessage('Is Strong Password')
+        } else {
+            setErrorMessage('Is Not Strong Password')
+        }}
+
+        function checkBlankForm(){}
